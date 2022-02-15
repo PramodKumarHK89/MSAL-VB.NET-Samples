63a7fde65ec6973eb4a9e695c11c8eac0c710871
PramodKumarHK89 <pramkum@microsoft.com>
2022-02-15 13:51:57 +0530

    MVC sample

---
+++ MVC/1-1-Sign-In/Vb.Net_MVC_Sign_In/Vb.Net_MVC_Sign_In/Controllers/AccountController.vb
@@ -0,0 +1,31 @@
+﻿Imports Microsoft.Owin.Security.Cookies
+Imports Microsoft.Owin.Security.OpenIdConnect
+Imports Microsoft.Owin.Security
+
+Public Class AccountController
+    Inherits Controller
+    
+    Public Sub SignIn()
+        ' Send an OpenID Connect sign-in request.
+        If Not Request.IsAuthenticated Then
+          HttpContext.GetOwinContext().Authentication.Challenge(New AuthenticationProperties() With { .RedirectUri = "/" }, 
+              OpenIdConnectAuthenticationDefaults.AuthenticationType)
+        End If
+    End Sub
+
+    Public Sub SignOut()
+      Dim callbackUrl As String = Url.Action("SignOutCallback", "Account", routeValues := Nothing, protocol := Request.Url.Scheme)
+
+      HttpContext.GetOwinContext().Authentication.SignOut(New AuthenticationProperties() With { .RedirectUri = callbackUrl }, 
+          OpenIdConnectAuthenticationDefaults.AuthenticationType, CookieAuthenticationDefaults.AuthenticationType)
+    End Sub
+
+    Public Function SignOutCallback() As ActionResult
+        If Request.IsAuthenticated Then
+            ' Redirect to home page if the user is authenticated.
+            Return RedirectToAction("Index", "Home")
+        End If
+
+        Return View()
+    End Function
+End Class
---
+++ MVC/1-1-Sign-In/Vb.Net_MVC_Sign_In/Vb.Net_MVC_Sign_In/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.vb" Inherits="Vb.Net_MVC_Sign_In.MvcApplication" Language="VB" %>
